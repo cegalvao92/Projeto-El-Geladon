a3530b5423d507a367e60d3f987f3425d6df1a6c
cegalvao92 <cegalvao13@gmail.com>
2022-04-26 18:37:29 -0300

    Atualização PaletaLista

--- cardapio-el-geladon/src/PaletaLista.jsx
+++ cardapio-el-geladon/src/PaletaLista.jsx
@@ -10,6 +10,15 @@ function PaletaLista() {
         setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
     }
 
+    const removerItem = (paletaIndex) => {
+        const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) -1 }
+        setPaletaSelecionada({...paletaSelecionada, ...paleta});
+    }
+    
+    const removeButton = (canRender, index) =>
+	    Boolean(canRender) && (<button className="Acoes__remover" onClick={() => removerItem(index)}>remover</button>);
+
+
     
     const badgeCounter = (canRender, index) =>
 	    Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index]} </span>);
@@ -26,8 +35,9 @@ function PaletaLista() {
                         <div className="PaletaListaItem__preco">R$ 10,00</div>
                         <div className="PaletaListaItem__descricao"> Quam vulputate dignissim suspendisse in est ante in nibh mauris.  </div>
                         <div className="PaletaListaItem__acoes Acoes">
-                        <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>adicionar</button>
+                        <button className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`} onClick={() => adicionarItem(index)}>adicionar</button>
 
+                        {removeButton(paletaSelecionada[index], index)}
                         </div>
                     </div>
                     <img className="PaletaListaItem__foto" src={paleta.foto} alt={`Paleta de ${paleta.sabor}`} />
